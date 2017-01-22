b4e6aa94aec9981884424f1966add8b458364bae
jphosom <jp_hosom@yahoo.com>
2017-01-21 21:56:08 -0800

    add agility code and demo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-alert("This is an alert I created in index.js!");
+// alert("This is an alert I created in index.js!");
+// var element = document.createElement('div');
+// var text = document.createTextNode('This is some text');
+// element.appendChild(text);
+// document.body.appendChild(element);
+
+
+//
+// Item prototype
+//
+var item = $$({}, '<li><span data-bind="content"/> <button>x</button></li>', '& span { cursor:pointer; }', {
+  'click span': function(){
+    var input = prompt('Edit to-do item:', this.model.get('content'));
+    if (!input) return;
+    this.model.set({content:input});
+  },
+  'click button': function(){
+    this.destroy();
+  }
+});
+
+//
+// List of items
+//
+var list = $$({}, '<div> <button id="new">New item</button> <ul></ul> </div>', {
+  'click #new': function(){
+    var newItem = $$(item, {content:'Click to edit'});
+    this.append(newItem, 'ul'); // add to container, appending at <ul>
+  }
+});
+
+$$.document.append(list);
